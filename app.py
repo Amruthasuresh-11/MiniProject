f2c4bcad2cb4cc18eef8a456871e228365eaec20
Amruthasuresh-11 <CHN24MC054@ceconline.edu>
2025-09-29 16:29:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,27 @@ import streamlit as st
 from home_page import home
 from data_entry_page import data_entry
 from result_page import result_page
-
-PAGES = {
-    "Home": home,
-    "Data Entry": data_entry,
-    "Result": result_page
-}
+from aptitude_test_page import aptitude_test
 
 # Initialize session state
 if "page" not in st.session_state:
     st.session_state["page"] = "Home"
+if "student_name" not in st.session_state:
+    st.session_state["student_name"] = ""
+if "etest_p" not in st.session_state:
+    st.session_state["etest_p"] = 0.0
+if "result" not in st.session_state:
+    st.session_state["result"] = None
+if "prob" not in st.session_state:
+    st.session_state["prob"] = None
 
-# Display the selected page
-page = PAGES[st.session_state["page"]]
-page()
+# Pages dictionary
+PAGES = {
+    "Home": home,
+    "Data Entry": data_entry,
+    "Aptitude Test": aptitude_test,
+    "Result": result_page
+}
+
+# Display current page
+PAGES[st.session_state["page"]]()
